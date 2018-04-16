c96e992025ae6d2593090e2ba9c0a6d464ad7b73
Jackoder <jackoder@qq.com>
2018-04-16 23:20:36 +0800

    modify name

---
+++ index.js
@@ -0,0 +1,5 @@
+import Font from './src/Font';
+import Color from './src/Color';
+module.exports={
+	Font, Color
+};
---
+++ src/Font.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Jackoder
+ * @Date: 2018-04-15 22:43:18
+ * @Last Modified by: Jackoder
+ * @Last Modified time: 2018-04-16 23:18:19
+ */
+'use strict';
+
+//未注释"加粗"的，都是正常字体 不加粗
+export const FONT1 = 18; //加粗
+export const FONT2 = 17; //加粗
+export const FONT3 = 18;
+export const FONT4 = 17;
+export const FONT5 = 14;
+export const FONT6 = 12;
+export const FONT7 = 11;
+export const FONT8 = 10;
+export const FONT9 = 24;
+export const FONT10 = 30;
+export const FONT11 = 14; //加粗
+export const FONT12 = 12; //加粗
+export const FONT13 = 16;
+export const FONT14 = 15;
